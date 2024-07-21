bf5c22511963d31b76a70463bafec9959c5ee099
Dobrovsky <miron.dobrovsky.work@gmail.com>
2024-07-21 15:21:32 +0300

    Initial Commit

---
+++ JSONGraphics/JSONGraphicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONGraphicsApp.swift
+//  JSONGraphics
+//
+//  Created by Dobrovsky on 21.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct JSONGraphicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
