1deae47f058795808e6e7c73d79e6358016c9cb9
Dobrovsky <miron.dobrovsky.work@gmail.com>
2024-07-23 10:25:38 +0300

    Сonnected API for gold and usd cost in my app, but UI didn't work wery well.

---
+++ JSONGraphics/BaseView.swift
@@ -0,0 +1,18 @@
+//
+//  BaseView.swift
+//  JSONGraphics
+//
+//  Created by Dobrovsky on 21.07.2024.
+//
+
+import SwiftUI
+
+struct BaseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BaseView()
+}
