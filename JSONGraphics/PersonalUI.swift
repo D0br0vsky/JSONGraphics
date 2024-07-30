f30802e356cc200967ab5a8a20eee42f06256983
Dobrovsky <miron.dobrovsky.work@gmail.com>
2024-07-30 18:23:07 +0300

    Add Bitcoin, Ethereum, Toncoin un my app! Add graphics to this value!

---
+++ JSONGraphics/PersonalUI.swift
@@ -0,0 +1,8 @@
+//
+//  PersonalUI.swift
+//  JSONGraphics
+//
+//  Created by Dobrovsky on 30.07.2024.
+//
+
+import Foundation
---
+++ JSONGraphics/SecondView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  JSONGraphics
+//
+//  Created by Dobrovsky on 21.07.2024.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text("Hi")
+    }
+}
+    
+
+#Preview {
+    SecondView()
+}
